aab01e66b399c60e6a3fdfa0e80aea8232a4510d
EULDONG-KIM <ked9181@naver.com>
2016-02-12 10:29:39 +0900

    * SelectActivity 복구

--- Main/app/src/main/java/com/hwc/main/SelectActivity.java
+++ Main/app/src/main/java/com/hwc/main/SelectActivity.java
@@ -1,7 +1,10 @@
 package com.hwc.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends Activity {
 
@@ -9,5 +12,16 @@ public class SelectActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        Button btnNFCInfo = (Button) findViewById(R.id.btnNFCInfo);
+        btnNFCInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(SelectActivity.this, NFCActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
